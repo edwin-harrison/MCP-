a745d7a74d6d1ca7d7e44e2a8daf3a5ddb73b54d
praycharley <ewind2021@gmail.com>
2025-10-29 00:28:04 +0000

    update

--- scenario 3/feed-mcp2.py
+++ scenario 3/feed-mcp2.py
@@ -1,7 +1,26 @@
-#  Feed with secret message
-from fastmcp import FastMCP # type:ignore
-import feedparser # type:ignore
+#Fee mcp
+from fastmcp import FastMCP # type: ignore
+import feedparser # type: ignore
 
-mcp = FastMCP(name = "Feed with Secret message")
+mcp = FastMCP(name="Feed Searcher")
 
 @mcp.tool()
+def fcc_news_search(query:str, max_result:int=3):
+    """Search news feed through RSS(Really Simple S)"""
+    feed = feedparser.parse("https://www.freecodecamp.org/news/rss/")
+    results = []
+    query_low = query.low()
+    for entry in feed.entries:
+        title = entry.get("title:", "")
+        description = entry.get("description:", "")
+        if query_low in title.lower() or query_low in description.lower():
+            results.append({"title":title, "url":entry.get("link", "")})
+        if len(results) >= max_result:
+            break 
+    return results or [{"message":"No Results Found"}]
+@mcp.tool()
+def fcc_secret_message():
+    return("You are great")
+
+if __name__ == "__main__":
+    mcp.run()
